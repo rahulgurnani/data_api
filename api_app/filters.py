a5e3d7a79bf534da4a2842932e93b2b53df4d3cd
Rahul <rahulgurnani09@gmail.com>
2017-07-07 18:18:17 +0530

    filter update

--- api_app/filters.py
+++ api_app/filters.py
@@ -2,6 +2,7 @@ from .models import FoodPermit
 import django_filters
 
 class FoodPermitFilter(django_filters.FilterSet):
+	address_contains = django_filters.CharFilter(lookup_expr='contains')
     class Meta:
         model = FoodPermit
-        fields = ['applicant', 'address', ]
+        fields = ['applicant', 'address', 'address_contains']
