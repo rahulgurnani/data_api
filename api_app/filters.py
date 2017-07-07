59961700e7af473ea6238fc5ec8076cc380efff2
Rahul <rahulgurnani09@gmail.com>
2017-07-07 22:48:19 +0530

    filters update

--- api_app/filters.py
+++ api_app/filters.py
@@ -4,7 +4,7 @@ import django_filters
 class FoodPermitFilter(django_filters.FilterSet):
     address_contains = django_filters.CharFilter(name='address', lookup_expr='contains')
     applicant_name_contains = django_filters.CharFilter(name='applicant', lookup_expr='contains')
-    expired_permits = django_filters.CharFilter(name='expirationdate', lookup_expr='expirationdate__lt')
+    expired_permits = django_filters.CharFilter(name='expirationdate', lookup_expr='year__lt')
     class Meta:
         model = FoodPermit
         fields = ['applicant', 'address', 'address_contains', 'expired_permits']
