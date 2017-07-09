9674e55619aa4a91e7a0003a44d296e19c6ce877
Rahul <rahulgurnani09@gmail.com>
2017-07-09 13:12:25 +0530

    get best truck update

--- api_app/views.py
+++ api_app/views.py
@@ -35,7 +35,7 @@ def add_entry(request):
 
 def predict_best(request):
 	if request.method == 'POST':
-		ans = find_nearest.get_truck_name(requestPOST.get('locationid'))
+		ans = find_nearest.get_truck_name(request.POST.get('locationid'))
 		#return render(request, 'predict_best.html', {'ans': ans})
 		return HttpResponse("Best truck : " + str(ans))
 	context = {'ans' : ''}
